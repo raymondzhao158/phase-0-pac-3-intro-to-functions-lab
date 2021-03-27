8642f19d941a0b7c9fba83d5e4d580cff2ca2cdd
raymond zhao <raymondzhao158@gmail.com>
2021-03-27 16:56:21 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+javascript
+function shout(string) {
+  return string.toUpperCase();
+}
+javascript
+function whisper(string) {
+  return string.toLowerCase();
+}
+function sayHiToGrandma (aWord) {
+    var cantUnswer = "I can't hear you!";
+    var yesUnswer = "YES INDEED!";
+    var lovUnswer = "I love you, too.";
+    if (aWord.toLowerCase(aWord) === aWord) {
+      return cantUnswer;
+    }
+    else if (aWord.toUpperCase(aWord) === aWord) {
+      return yesUnswer;
+    }
+    else if ("I love you, Grandma." === aWord) {
+      return lovUnswer
+    }
+  }
+  function logShout (aWord) {
+    console.log(aWord.toUpperCase( ));
+  }
+  function logWhisper (aWord) {
+    console.log(aWord.toLowerCase( ));
+  }
